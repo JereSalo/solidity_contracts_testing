dcda152df5b095dee1771c81c1d0e9acd248e601
JereSalo <jeresalo17@gmail.com>
2024-11-08 15:14:54 -0300

    modifiy call contract

--- src/Caller_Target.sol
+++ src/Caller_Target.sol
@@ -2,24 +2,24 @@
 pragma solidity ^0.8.0;
 
 contract Caller {
-    // This function uses the low-level `call` to interact with another contract
-    function callAnotherContract(address _target, bytes memory _data) public payable returns (bool, bytes memory) {
-        // Use `call` to send the data and optionally send ether
-        (bool success, bytes memory result) = _target.call{value: msg.value}(_data);
-        
-        // Check if the call succeeded
-        require(success, "External call failed");
-        
-        // Return the result of the call
-        return (success, result);
+    // This function uses `call` to call `setValue` on the Target contract
+    function callSetValue(address _target, uint _value) public returns (bool) {
+        // Encode the function signature and parameter for `setValue`
+        bytes memory data = abi.encodeWithSignature("setValue(uint256)", _value);
+
+        // Use low-level call to call the Target contract
+        (bool success, ) = _target.call(data);
+
+        // Return whether the call succeeded
+        return success;
     }
 }
 
 contract Target {
     uint public value;
-    
-    // A function to be called
-    function setValue(uint _value) public payable {
+
+    // This function simply sets the `value` variable
+    function setValue(uint _value) public {
         value = _value;
     }
 }
